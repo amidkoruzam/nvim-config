14006b6be4c617a86adb863bf7bc55c3e0f8eff4
Dmitrii Mazurok <tmazurok@gmail.com>
2023-05-11 20:06:32 +0300

    add rose-pine

--- modules/packer.lua
+++ modules/packer.lua
@@ -3,4 +3,11 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  vim.cmd('colorscheme rose-pine')
 end)
