3829b57c50c85bad5d6487cc60328f276e413353
Dmitrii Mazurok <tmazurok@gmail.com>
2023-05-13 11:43:36 +0300

    add lsp

--- modules/packer.lua
+++ modules/packer.lua
@@ -14,4 +14,23 @@ return require('packer').startup(function(use)
   use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use 'mbbill/undotree'
   use 'tpope/vim-fugitive'
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v2.x',
+	  requires = {
+		  {'neovim/nvim-lspconfig'},             
+		  {
+		  'williamboman/mason.nvim',
+		  run = function()
+			  pcall(vim.cmd, 'MasonUpdate')
+		  end,
+	  },
+	  {'williamboman/mason-lspconfig.nvim'}, 
+
+	  {'hrsh7th/nvim-cmp'},
+	  {'hrsh7th/cmp-nvim-lsp'},
+	  {'L3MON4D3/LuaSnip'},
+  }
+}
 end)
