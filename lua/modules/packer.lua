4cbbc70828eb7349d21c0a94cb2621d65f1dd472
Dmitrii Mazurok <tmazurok@gmail.com>
2023-05-22 23:24:09 +0300

    add softtabstop

--- lua/modules/packer.lua
+++ lua/modules/packer.lua
@@ -31,6 +31,8 @@ return require('packer').startup(function(use)
 	  {'hrsh7th/nvim-cmp'},
 	  {'hrsh7th/cmp-nvim-lsp'},
 	  {'L3MON4D3/LuaSnip'},
-  }
+  },
+
+  use 'preservim/nerdtree'
 }
 end)
--- lua/modules/set.lua
+++ lua/modules/set.lua
@@ -9,3 +9,4 @@ vim.opt.hlsearch = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
